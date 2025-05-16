013e52f56b79a6758363d37970174ef7af70307a
Carlos <carlosmal1502@gmail.com>
2025-05-15 22:30:36 -0600

    Add first trigger

--- DML.sql
+++ DML.sql
@@ -29,6 +29,38 @@
 -- ## TRIGGERS AUTO
 -- ##################################################
 
+CREATE TRIGGER TR_AUTO_AF1
+ON USUARIOS.AUTOMOVIL
+FOR INSERT, UPDATE
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	IF EXISTS(
+		SELECT ID_AUTOMOVIL 
+		FROM USUARIOS.AUTOMOVIL
+		WHERE ID_USUARIO IN (SELECT ID_USUARIO FROM inserted)
+		GROUP BY ID_USUARIO
+		HAVING COUNT(*)>2
+	)
+	BEGIN
+		RAISERROR('El maximo de automoviles por conductor es 2',16,1);
+		ROLLBACK TRANSACTION;
+	END
+
+	IF EXISTS(
+		SELECT I.ID_AUTOMOVIL
+		FROM inserted AS I
+		JOIN USUARIOS.AUTOMOVIL AS A
+			ON I.ID_AUTOMOVIL=A.ID_AUTOMOVIL
+		WHERE I.ID_USUARIO!=A.ID_USUARIO
+	)
+	BEGIN
+		RAISERROR('El vehiculo ya esta registrado para otro conductor',16,1);
+		ROLLBACK TRANSACTION;
+	END
+END;
+
 -- ##################################################
 -- ## TRIGGERS CLIENTE
 -- ##################################################
